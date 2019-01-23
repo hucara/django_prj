0c2b52093f02951909541395abbe17594809274e
hucara <hucara@users.noreply.github.com>
2019-01-23 19:55:50 +0100

    more pep8 corrections

--- fakenews/post_example.py
+++ fakenews/post_example.py
@@ -1,26 +1,28 @@
 import requests
-import datetime
-import sys, json
+import sys
 
 
 def send():
 
     reports = []
-    reports.append({'title' : 'post title',
-                'author' : 'post author',
-                'content' : 'post content',
-                'is_train' : True,
-                'label' : 0,
-                })
-    reports.append({'title' : 'post title',
-                'author' : 'post author',
-                'content' : 'post content',
-                'is_train' : True,
-                'label' : 0,
-                })
+    reports.append({
+        'title': 'post title', 'author': 'post author',
+        'content': 'post content', 'is_train': True, 'label': 0
+        })
 
-    headers = {'Content-Type': 'application/json', 'Accept':'application/json'}
-    r = requests.post('http://127.0.0.1:8000/api/reports/', json = reports, headers = headers)
+    reports.append({
+        'title': 'post title', 'author': 'post author',
+        'content': 'post content', 'is_train': True, 'label': 0
+        })
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+    r = requests.post(
+        'http://127.0.0.1:8000/api/reports/',
+        json=reports, headers=headers
+    )
     sys.stdout.write(r.text)
 
 
